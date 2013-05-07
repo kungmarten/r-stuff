b9ab89176e4bff9dce888f1549cd2b530b77d183
Mårten Lindblad <marten.lindblad@gmail.com>
2013-05-07 16:51:13 +0200

    Fixet transition calculation

--- testrank.R
+++ testrank.R
@@ -10,7 +10,8 @@ inacmat <- zeros(size(trmat, 1), size(trmat, 2))
 while (cc > 0) {
    nmat = trmat
    nmat[cc] = (trmat[cc] * 1.1)
-   nmat = nmat/colSums(nmat)
+   nmat = nmat/rowSums(nmat)
+   nmat = t(nmat)
    prv = pagerank (nmat, 0.95, 0.0001);
    inacmat[cc] = prv[rankpoint];
    cc = cc - 1;
