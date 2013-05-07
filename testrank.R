ab423c67f3f8c4cfe62b0d7db36369671fdcf154
Mårten Lindblad <marten.lindblad@gmail.com>
2013-05-07 16:16:42 +0200

    ranktester

---
+++ testrank.R
@@ -0,0 +1,17 @@
+# Number of runs (one for each cell)
+cc = length(trmat)
+
+# let's measure the inactivity rank
+rankpoint = 11
+
+# Create empty matrix
+inacmat <- zeros(size(trmat, 1), size(trmat, 2))
+
+while (cc > 0) {
+   nmat = trmat
+   nmat[cc] = (trmat[cc] * 1.1)
+   nmat = nmat/colSums(nmat)
+   prv = pagerank (nmat, 0.95, 0.0001);
+   inacmat[cc] = prv[rankpoint];
+   cc = cc - 1;
+}
